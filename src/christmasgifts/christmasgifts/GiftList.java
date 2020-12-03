847268198feef4944c148d539da67264c108ceeb
Maxi <herreramaxi@gmail.com>
2020-12-03 00:14:22 +0000

    deleting giftlist file

--- src/christmasgifts/christmasgifts/GiftList.java
+++ src/christmasgifts/christmasgifts/GiftList.java
@@ -17,7 +17,7 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors; 
 
 /**
  *
